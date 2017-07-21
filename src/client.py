4073ebaefe1629e877695119e35cfb512abf2fa4
VIthulan <mvvithulan@gmail.com>
2017-07-21 18:16:04 +0530

    remove unwanted lines

--- src/client.py
+++ src/client.py
@@ -1,7 +1,6 @@
 import core as client
 
 client.make_connection_with_key('10.3.11.164', 'root', '/home/vithulan/ssh-keys/tmp_id_rsa', '')
-# client.make_connection('10.10.10.121', 'root', '1ring2rule')
 client.make_dir('/root/', 'sherlock')
 client.upload_to_directory('sherlock', 'SF-00004', '../files/testsftp2')
 client.close_connection()
